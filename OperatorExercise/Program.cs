f83e0d60bb5e4d3c9e2369f2b70ed61ab313a3bf
Zezuz <mczanaty92@gmail.com>
2021-02-16 20:11:33 -0600

    added some steps

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //explicit typing -
+            string variableName = "Hello";
+
+            //inferred typing -
+            var varName = "Hello";
+
+
+            // x += 12 is ( x = x+12)
+            // x -= 11 is ( x = x-12)
+            // x /= 12 is ( x = x/12)
+            var a = 17;
+            var b = 4;
+
+            var div = a / b;
+            var mod = a % b;
+
+            Console.WriteLine($"{a}/{b} is {div} remainder {mod}");
+
+        }
+    }
+}
